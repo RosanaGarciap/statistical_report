a3a8abf1706e3340183a4837722d34bcdcf45c9b
RosanaGarciap <97164329+RosanaGarciap@users.noreply.github.com>
2025-02-08 07:05:53 -0400

    Add files via upload
    Main program with customized functions.

---
+++ main.py
@@ -0,0 +1,46 @@
+####################################################################
+# ROSANA GARCIA
+# CSE 230
+# SRINT 2: 
+# ROSANA GARCIA
+####################################################################
+
+#   IMPORT PYTHON LIBRARIES
+import pandas as pd
+import os
+from datetime import date
+from datetime import datetime
+
+#   IMPORT CUSTOMIZED SOURCE FUNCTIONS
+from searching import *
+
+DISEASES = ["Arthritis", "Asthma", "Cancer", "Diabetes", "Hypertension", "Obesity"]
+
+# LOAD DATAFRAME FROM CSV FILE
+cwd = os.getcwd()
+filename = "healthcare_dataset.csv"
+file_path = os.path.join(cwd, filename)
+df = pd.read_csv(file_path) 
+
+# EXTRACTING DATA
+
+# Convert string date instance to datetime instance
+df["Date of Admission"] = pd.to_datetime(df['Date of Admission'])
+
+
+# Select subdataframe from original that contains the data we need to work with 
+testing_data = df[["Medical Condition", "Gender", "Hospital", "Date of Admission"]]
+
+# Seting current date and adjust date format to Year/Month/Day
+
+current_date = date.today().strftime("%d/%m/%Y")
+starting_date = datetime.strptime("2023/01/01", '%Y/%m/%d')
+starting_date_case2 = datetime.strptime("2024/01/01", '%Y/%m/%d')
+pdf_title = "Healthcare Data Analysis Report"
+
+#Selecting plotting data 
+plots = medical_center_assistence(testing_data, DISEASES, starting_date)
+show_plot(plots, "Diseases quantity report from {} to {}".format(starting_date.strftime("%d/%m/%Y"), current_date))
+
+#How many cases of Female and male patients with Diabetes we have had since 2023? 
+all_that(testing_data, DISEASES[3], starting_date, starting_date_case2)
---
+++ searching.py
@@ -0,0 +1,128 @@
+#######################################################################
+# Filtering functions 
+#
+#
+#######################################################################
+
+# Importing libraries
+from pypdf import PdfWriter
+from datetime import date
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+# Function Name: all_that
+# INPUT: 
+#   df: dataset
+#   _columname: column name we want to analyse
+#   that_condition: single value constrain we want to analize
+# OUTPUT:
+#   amount: number of cases (rows) that satisfy the given condition
+
+def all_that(df, _columname, that_condition):
+    df1 = df.loc[(df[_columname] == that_condition) ]
+    amount = df1.value_counts()
+    return amount
+
+# Function Name: all_that
+# INPUT: 
+#   df: dataset
+#   _columname: string of columns names we want to analyse
+#   that_conditions: array of single values we want to analize
+#   from_date: date from which start to analyse
+# OUTPUT:
+#   amount: number of cases (rows) that satisfy the given condition
+
+def all_that(df, _columname, from_date, to_date = date.today().strftime("%Y/%m/%d") ): 
+    plottings = []
+
+    general_result = df.loc[(df["Medical Condition"] == _columname)  & ( df["Date of Admission"] >= from_date) & ( df["Date of Admission"] <= to_date) ]
+    female_result = general_result.loc[(df["Gender"] == "Female")]
+    male_result = general_result.loc[(df["Gender"] == "Male")]
+
+    female_general_result = female_result.groupby('Date of Admission', as_index=False).agg('count')
+    male_general_result = male_result.groupby('Date of Admission', as_index=False).agg('count')
+    
+    plottings.append(female_general_result[["Date of Admission", "Gender"]])
+    plottings.append(male_general_result[["Date of Admission", "Gender"]])
+    
+    show_spectroscopy(plottings)
+ 
+# Function Name: medical_center_assistence 
+# INPUT: 
+#   df: dataset
+#   _columnames: array of columns names we want to analyse
+#   that_conditions: array of single values we want to analize
+#   from_date: date from which start to analyse
+# OUTPUT:
+#   amount: number of cases (rows) that satisfy the given condition
+#
+# DESCRIPTION: Returns all the cases attended for every disease from
+# a given date until current date
+
+def medical_center_assistence(df, diseases, from_date):
+    plottings = {}
+    #print(hospital_centers.iloc[0])
+
+    for disease in diseases:
+         
+        #print(disease)
+        result = df.loc[(df["Medical Condition"] == disease) & ( df["Date of Admission"] >= from_date)]
+        if not result.empty:
+            row_number = result["Medical Condition"].count()
+            if row_number > 2:
+                #print (row_number)
+                plottings[disease] = row_number
+    return plottings
+
+
+def show_plot(plots, title):
+    with PdfPages('barplot_.pdf') as pdf:
+        header_values = list(plots.keys())
+        #print(header_values)
+        height_values = list(plots.values())
+        axe_y = [250, 500, 550, 600, 650, 700]
+        print(height_values)
+
+        plt.figure()
+        plt.bar(header_values, height_values)
+
+        # plot's title
+        plt.title(title)
+
+        # X and Y labels
+        plt.xlabel("Medical Condition")
+        plt.ylabel("Number of Admission Cases")
+
+        for i in range(0, len(header_values)):
+            plt.text(header_values[i], height_values[i], str(height_values[i]), ha="center" )
+        # visualizing the plot
+        #plt.show()
+        #Saving plot unto pdf file
+        pdf.savefig()
+        #close object
+        plt.close()
+
+#
+# INPUT: Dataframe of 2 columns
+#
+#
+def show_spectroscopy(scattered_plots):
+    with PdfPages('timespan_analysis_plot.pdf') as pdf:
+        y_values = list( scattered_plots[0]["Gender"])
+        x_values = list( scattered_plots[0]["Date of Admission"])
+        y2_values = list( scattered_plots[1]["Gender"])
+        x2_values = list( scattered_plots[1]["Date of Admission"])
+
+        fig, ax = plt.subplots(1)
+        fig.set_size_inches(14,6)
+
+        #print("###########################################")
+        #get columns names for labels
+        ax.plot(x_values, y_values, label="Female")           
+        ax.plot(x2_values, y2_values,label="Male")   
+        ax.legend()        
+        ax.grid(True)
+        pdf.savefig()
+        plt.show()
+    plt.close()
